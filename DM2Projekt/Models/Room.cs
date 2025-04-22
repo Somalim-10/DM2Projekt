0561be4e92177e1500ffcaf777ded80f8a1abe9d
Somalim-10 <mohmed43001@gmail.com>
2025-04-22 12:11:20 +0200

    Room klassen er lavaet, samt tilføjet attribute.

---
+++ DM2Projekt/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DM2Projekt.Models
+{
+    public class Room
+    {
+   
+        //classNameId
+        //ID
+        // "d" I Id skal være med et lille "d", da der kan opstå konflikter i projektet. Så ved den også at det en PrimaryKey.
+        public int RoomId { get; set; } 
+
+        public string RoomName { get; set; }
+        public int Capacity { get; set; } // Number of people
+        public string RoomType { get; set; }
+        public bool CanBeShared{ get; set; }
+
+    }
+}
