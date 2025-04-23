a89344a16267acd356675a04cb3642cc5470a731
Abdi22-7 <ah8808573@gmail.com>
2025-04-23 12:17:32 +0200

    har lavet en booking class

---
+++ DM2Projekt/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿namespace DM2Projekt.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        
+        public int GroupId { get; set; }
+        public int RoomId { get; set; }
+        public int CreateByUserId { get; set; }
+        public int SmartboardId { get; set; }
+
+        // nav properties
+        public Room Room { get; set; }
+        public User CreatedByUser { get; set; }
+        public Group Group{ get;set; }
+        public Smartboard Smartboard { get; set; }
+
+    }
+}
