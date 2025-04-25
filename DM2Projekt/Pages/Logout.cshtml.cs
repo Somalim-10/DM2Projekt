718492e91425d80c570d8f17dab604d0dcd0f684
Anton Wessel <130216564+antonwessel@users.noreply.github.com>
2025-04-25 17:23:38 +0200

    logout function

---
+++ DM2Projekt/Pages/Logout.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DM2Projekt.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public IActionResult OnPost()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Index");
+        }
+    }
+}
