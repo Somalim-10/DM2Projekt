3ce50f6161b2d6af99c9f09819c5a5a4fbd89299
Anton Wessel <130216564+antonwessel@users.noreply.github.com>
2025-04-25 17:43:56 +0200

    Adjust Create Page

--- DM2Projekt/Pages/Users/Create.cshtml.cs
+++ DM2Projekt/Pages/Users/Create.cshtml.cs
@@ -1,56 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using DM2Projekt.Data;
 using DM2Projekt.Models;
 using DM2Projekt.Models.Enums;
 
-namespace DM2Projekt.Pages.Users
+namespace DM2Projekt.Pages.Users;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly DM2ProjektContext _context;
+
+    public CreateModel(DM2ProjektContext context)
     {
-        public IEnumerable<SelectListItem> RoleOptions { get; set; } = [];
+        _context = context;
+    }
 
+    [BindProperty]
+    public User User { get; set; } = default!;
 
-        private readonly DM2Projekt.Data.DM2ProjektContext _context;
+    public IEnumerable<SelectListItem> RoleOptions { get; set; } = [];
 
-        public CreateModel(DM2Projekt.Data.DM2ProjektContext context)
+    public IActionResult OnGet()
+    {
+        RoleOptions = Enum.GetValues(typeof(Role))
+                          .Cast<Role>()
+                          .Select(r => new SelectListItem
+                          {
+                              Value = ((int)r).ToString(),
+                              Text = r.ToString()
+                          });
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
-            _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-            RoleOptions = Enum.GetValues(typeof(Role))
-                              .Cast<Role>()
-                              .Select(r => new SelectListItem
-                              {
-                                  Value = ((int)r).ToString(),
-                                  Text = r.ToString()
-                              });
-
             return Page();
         }
 
-        [BindProperty]
-        public User User { get; set; } = default!;
+        _context.User.Add(User);
+        await _context.SaveChangesAsync();
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.User.Add(User);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
+        return RedirectToPage("./Index");
     }
 }
