a3a84956fa7b504d93ebc01825681d1c50aa3b06
Anton Wessel <130216564+antonwessel@users.noreply.github.com>
2025-04-26 19:23:22 +0200

    Logic for Groups

--- DM2Projekt/Pages/Groups/Details.cshtml.cs
+++ DM2Projekt/Pages/Groups/Details.cshtml.cs
@@ -1,43 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DM2Projekt.Data;
 using DM2Projekt.Models;
 
-namespace DM2Projekt.Pages.Groups
+namespace DM2Projekt.Pages.Groups;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly DM2ProjektContext _context;
+
+    public DetailsModel(DM2ProjektContext context)
     {
-        private readonly DM2Projekt.Data.DM2ProjektContext _context;
+        _context = context;
+    }
 
-        public DetailsModel(DM2Projekt.Data.DM2ProjektContext context)
+    public Group Group { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
-        }
-
-        public Group Group { get; set; } = default!;
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var group = await _context.Group.FirstOrDefaultAsync(m => m.GroupId == id);
-
-            if (group is not null)
-            {
-                Group = group;
-
-                return Page();
-            }
-
             return NotFound();
         }
+
+        // find the group
+        var group = await _context.Group.FirstOrDefaultAsync(m => m.GroupId == id);
+
+        if (group != null)
+        {
+            Group = group;
+            return Page();
+        }
+
+        return NotFound();
     }
 }
