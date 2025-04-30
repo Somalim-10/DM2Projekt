433c68e5d107b14cf79c0f528056d09a0742ff81
Anton Wessel <130216564+antonwessel@users.noreply.github.com>
2025-04-30 12:01:45 +0200

    can accept/decline invites

--- DM2Projekt/Pages/GroupInvitations/Index.cshtml.cs
+++ DM2Projekt/Pages/GroupInvitations/Index.cshtml.cs
@@ -1,31 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DM2Projekt.Data;
 using DM2Projekt.Models;
 
-namespace DM2Projekt.Pages.GroupInvitations
+namespace DM2Projekt.Pages.GroupInvitations;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly DM2ProjektContext _context;
+
+    public IndexModel(DM2ProjektContext context)
     {
-        private readonly DM2Projekt.Data.DM2ProjektContext _context;
+        _context = context;
+    }
 
-        public IndexModel(DM2Projekt.Data.DM2ProjektContext context)
+    public IList<GroupInvitation> GroupInvitation { get; set; } = [];
+
+    [BindProperty]
+    public int ActionInvitationId { get; set; } // for Accept/Decline form binding
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var userId = HttpContext.Session.GetInt32("UserId");
+        var userRole = HttpContext.Session.GetString("UserRole");
+
+        if (userId == null || userRole != "Student")
+            return RedirectToPage("/Index");
+
+        GroupInvitation = await _context.GroupInvitation
+            .Include(i => i.Group)
+            .Where(i => i.InvitedUserId == userId && i.IsAccepted == null)
+            .ToListAsync();
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAcceptAsync()
+    {
+        var userId = HttpContext.Session.GetInt32("UserId");
+        if (userId == null)
+            return RedirectToPage("/Login");
+
+        var invite = await _context.GroupInvitation
+            .Include(i => i.Group)
+            .FirstOrDefaultAsync(i =>
+                i.InvitationId == ActionInvitationId &&
+                i.InvitedUserId == userId &&
+                i.IsAccepted == null);
+
+        if (invite == null)
+            return RedirectToPage();
+
+        // Accept the invitation
+        invite.IsAccepted = true;
+
+        // Add user to group
+        var userGroup = new UserGroup
         {
-            _context = context;
-        }
+            UserId = userId.Value,
+            GroupId = invite.GroupId
+        };
+        _context.UserGroup.Add(userGroup);
 
-        public IList<GroupInvitation> GroupInvitation { get;set; } = default!;
+        await _context.SaveChangesAsync();
+        return RedirectToPage();
+    }
 
-        public async Task OnGetAsync()
-        {
-            GroupInvitation = await _context.GroupInvitation
-                .Include(g => g.Group)
-                .Include(g => g.InvitedUser).ToListAsync();
-        }
+    public async Task<IActionResult> OnPostDeclineAsync()
+    {
+        var userId = HttpContext.Session.GetInt32("UserId");
+        if (userId == null)
+            return RedirectToPage("/Login");
+
+        var invite = await _context.GroupInvitation
+            .FirstOrDefaultAsync(i =>
+                i.InvitationId == ActionInvitationId &&
+                i.InvitedUserId == userId &&
+                i.IsAccepted == null);
+
+        if (invite == null)
+            return RedirectToPage();
+
+        // Decline the invitation
+        invite.IsAccepted = false;
+
+        await _context.SaveChangesAsync();
+        return RedirectToPage();
     }
 }
