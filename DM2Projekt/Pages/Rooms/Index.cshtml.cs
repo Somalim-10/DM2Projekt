90688150f084aea08ff5958b868dece991166d42
Somalim-10 <mohmed43001@gmail.com>
2025-05-01 21:13:24 +0200

    tilføjet filtering til rooms

--- DM2Projekt/Pages/Rooms/Index.cshtml.cs
+++ DM2Projekt/Pages/Rooms/Index.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using DM2Projekt.Data;
 using DM2Projekt.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DM2Projekt.Pages.Rooms;
 
@@ -15,17 +17,50 @@ public class IndexModel : PageModel
         _context = context;
     }
 
+    public List<SelectListItem> RoomOptions { get; set; } = new(); // holds all room choices
+
+    [BindProperty(SupportsGet = true)]
+    public int? RoomId { get; set; }
+    public int? SelectedRoomId => RoomId;
+
+
+
     public IList<Room> Room { get; set; } = default!;
 
     public async Task<IActionResult> OnGetAsync()
     {
+
+        RoomOptions = await _context.Room
+          .OrderBy(r => r.RoomName)
+          .Select(r => new SelectListItem
+          {
+              Value = r.RoomId.ToString(),
+              Text = r.RoomName
+          })
+          .ToListAsync();
+   
+        var query = _context.Room.AsQueryable();
+         
+       
+        if (RoomId.HasValue)
+        {
+            query = query.Where(b => b.RoomId == RoomId.Value);
+        }
+
+
+        if (RoomId.HasValue)
+        {
+            query = query.Where(b => b.RoomId == RoomId.Value);
+        }
+
+
         var userId = HttpContext.Session.GetInt32("UserId");
         if (userId == null) // if not logged in
         {
             return RedirectToPage("/Login");
         }
 
-        Room = await _context.Room.ToListAsync();
+        Room = await query.ToListAsync();
         return Page();
     }
 }
