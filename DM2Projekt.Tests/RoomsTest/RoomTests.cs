93191c85d94d686843ab30adcd126e0485218f58
Somalim-10 <mohmed43001@gmail.com>
2025-05-07 13:27:23 +0200

    test method til filterings

--- DM2Projekt.Tests/RoomsTest/RoomTests.cs
+++ DM2Projekt.Tests/RoomsTest/RoomTests.cs
@@ -8,22 +8,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DM2Projekt.Tests.RoomsTest
 {
-
+    [TestClass]  
     public class RoomTests
     {
-    private DM2ProjektContext GetInMemoryContext()
-    {
-        var options = new DbContextOptionsBuilder<DM2ProjektContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
-            .Options;
+        
+        private DM2ProjektContext GetInMemoryContext()
+        {
+            var options = new DbContextOptionsBuilder<DM2ProjektContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()) 
+                .Options;
 
-        return new DM2ProjektContext(options);
-    }
+            return new DM2ProjektContext(options);
+        }
 
+        // Testmetode for at teste FilterRooms
         [TestMethod]
         public void FilterRooms_Filters_By_RoomId()
         {
-            // Arrange
+            // Arrange: Opretter en in-memory database og tilføjer nogle rum
             var context = GetInMemoryContext();
             context.Room.AddRange(
                 new Room { RoomId = 1, RoomName = "Lokale A" },
@@ -33,12 +35,11 @@ namespace DM2Projekt.Tests.RoomsTest
 
             var model = new IndexModel(context);
 
-            // Act
+         
             var filtered = model.FilterRooms(context.Room.AsQueryable(), 1).ToList();
 
-            // Assert
-            Assert.AreEqual(1, filtered.Count);
-            Assert.AreEqual("Lokale A", filtered[0].RoomName);
+            Assert.AreEqual(1, filtered.Count); 
+            Assert.AreEqual("Lokale A", filtered[0].RoomName); 
         }
     }
 }
