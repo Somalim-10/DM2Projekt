5c142f12c91d335116ba213b2d0b4731ce9cdcd7
antonwessel <anton62045@gmail.com>
2025-05-08 15:00:34 +0200

    Add ReminderSent to Booking-model

---
+++ DM2Projekt/Migrations/20250508125918_ReminderSent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DM2Projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReminderSent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ReminderSent",
+                table: "Booking",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReminderSent",
+                table: "Booking");
+        }
+    }
+}
