c6517d64603e9e995429981a0ca89f8c4517125a
antonwessel <anton62045@gmail.com>
2025-05-16 14:06:19 +0200

    update Room and SeedData

--- DM2Projekt/Models/Enums/Floor.cs
+++ DM2Projekt/Models/Enums/Floor.cs
@@ -6,6 +6,4 @@ public enum Floor
     First = 1,
     Second = 2,
     Third = 3,
-
-
 }
--- DM2Projekt/Models/Room.cs
+++ DM2Projekt/Models/Room.cs
@@ -1,20 +1,19 @@
 ﻿using DM2Projekt.Models.Enums;
 using System.ComponentModel.DataAnnotations;
 
-
 namespace DM2Projekt.Models;
 
 // room model
 public class Room
-{ 
-  [Key]
+{
+    [Key]
     public int RoomId { get; set; }
 
     [Required]
     public string RoomName { get; set; }
 
     public RoomType RoomType { get; set; } // classroom or meeting room
-    
+
     [Url]
     [RegularExpression(@".*\.(jpg|jpeg|png|gif|bmp|webp)$", ErrorMessage = "URL skal pege på et billede (.jpg, .jpeg, .png, .gif, .bmp, .webp)")]
     public string? ImageUrl { get; set; }
@@ -25,12 +24,6 @@ public class Room
     [Required(ErrorMessage = "Etage skal vælges.")]
     public Floor Floor { get; set; }
 
-
-    // User UI
-    public string? BuildingName { get; set; }
-    public string? FloorName { get; set; }
     // bookings for this room
     public ICollection<Booking> Bookings { get; set; } = [];
 }
-
-
